28c620e2bcb3a2da6d421167d4b1f1aeaa8c5dda
lamtota40 <lamtota40@gmail.com>
2023-01-31 18:55:55 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -84,7 +84,7 @@ tar xvf $ARCHIVE
 rm $ARCHIVE
 sudo chmod +x ngrok
 
-echo "Running ngrok for ARCH $(uname -m) . . ."
+echo "Running ngrok v3 stable for ARCH $(uname -m) . . ."
 systemctl enable ngrok.service
 systemctl start ngrok.service
 echo "Wait 10s…"
@@ -93,7 +93,7 @@ echo "Finish… to check status NGROK: http://127.0.01:4040"
 echo "To setting configuration: ngrok.yml"
 echo -e "To disable NGROK service on startup:\n systemctl disable ngrok.service"
 echo -e "To stop service NGROK:\n systemctl stop ngrok.service"
-echo -e "To change authtoken:\n /opt/ngrok/ngrok config add-authtoken 2J8ncba…"
+echo -e "To change authtoken:\n /opt/dirngrok/ngrok config add-authtoken 2J8ncba…"
 
 sudo apt install gparted -y
 sudo apt install grml-rescueboot -y
