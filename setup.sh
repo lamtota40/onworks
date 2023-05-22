eca884aac564511122a7a045499fcb480bd5a7bb
lamtota40 <lamtota40@gmail.com>
2023-05-22 16:49:00 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -97,7 +97,7 @@ systemctl enable ngrok.service
 systemctl start ngrok.service
 
 sudo apt install grml-rescueboot -y
-sudo wget -O /boot/grml download.grml.org/grml64-full_2022.11.iso -O grml.iso
+sudo wget -O /boot/grml/grml.iso download.grml.org/grml64-full_2022.11.iso
 sudo bash -c "echo 'CUSTOM_BOOTOPTIONS=\"ssh=pas123 vnc=pas123 dns=8.8.8.8,8.8.4.4 netscript=raw.githubusercontent.com/lamtota40/tes/main/setup-ngrok.sh startx toram\"' >> /etc/default/grml-rescueboot"
 echo -ne '\n' | sudo add-apt-repository ppa:danielrichter2007/grub-customizer
 echo -ne '\n' | sudo apt update
